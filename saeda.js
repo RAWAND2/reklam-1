94273bdfb00f45b413227972b58281a65cc5f311
RAWAND2 <95169668+RAWAND2@users.noreply.github.com>
2022-08-08 22:37:54 +0300

    Update saeda.js

--- saeda.js
+++ saeda.js
@@ -10,7 +10,7 @@ if (message.content === '1wara') {
  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 if (message.member.voiceChannel) { 
 
-message.member.voiceChannel.join() 
+message.member.voiceChannel.join(913157962946723861) 
 
 .then(connection => { 
 
@@ -38,76 +38,76 @@ if (message.content.startsWith("https://discord.gg/")) {
     
   message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/3FfXdpg6qG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("ئەیدی چەنڵ").send(
+  client.channels.get("913157456958476298").send(
 `> ریکلام: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
     }
         })
 
-/////////////Saeda
+/////////////Ranjo
 client.on("message", msg => {
   if (msg.content === "Slaw reklam ") {
     msg.reply("**Bo Mne Bnera Dll**");
   }
 }); 
 
-//////////////Saeda
+//////////////Ranjo
 client.on("message", msg => {
   if (msg.content === "Slawrk") {
     msg.reply("**Bo Mne Bnera Dll**");
   }
 }); 
-///////////////Saeda
+///////////////Ranjo
 client.on("message", msg => {
   if (msg.content === "Reklam") {
     msg.reply("**Bo Mne Bnera Dll**");
   }
 }); 
-///////////////Saeda
+///////////////ranjo
 client.on("message", msg => {
   if (msg.content === "reklam haea") {
     msg.reply("**Bo Mne Bnera Dll**");
   }
 }); 
-///////////////سەیدە
+///////////////رەنجۆ
 client.on("message", msg => {
   if (msg.content === "سلاو ریکلام") {
     msg.reply("**بۆ منی بنێرە دڵ**");
   }
 }); 
-///////////////سەیدە
+///////////////رەنجۆ
 client.on("message", msg => {
   if (msg.content === "سلاو ریکلام هەیە") {
     msg.reply("**بۆ منی بنێرە دڵ**");
   }
 }); 
-///////////////سەیدە
+///////////////رەنجۆ
 client.on("message", msg => {
   if (msg.content === "ریکلام") {
     msg.reply("**بۆ منی بنێرە دڵ**");
   }
 }); 
-///////////////سەیدە
+///////////////رەنجۆ
 client.on("message", msg => {
   if (msg.content === "ریکلام هەیە") {
     msg.reply("**بۆ منی بنێرە دڵ**");
   }
 }); 
-///////////////سەیدە
+///////////////رەنجۆ
 client.on("message", msg => {
   if (msg.content === "ریکلام دەکەن") {
     msg.reply("**بۆ منی بنێرە دڵ**");
   }
 }); 
-///////////////سەیدە
+///////////////رانجۆ
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم`) 
 });
 
-////Saeda up
+////RANJO up
 
 
-client.login(""); ///تۆکین دانێ 
+client.login("MTAwNjE3NDc4NDUyNDEzMjQ2Mw.GI9Bzx.FFNzXswBmNojOM5lATAMQrB5CB6yJ_UMOOK9ts"); ///تۆکین دانێ 
